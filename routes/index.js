b2e2898283480fd7881cd78cea3b0a1933ab8172
JaeyeonKIM <foodsksms@gmail.com>
2019-11-30 20:43:48 +0900

    controller에서 excel로드

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+const XLSX = require('xlsx');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/excel', (req, res, next) => {
+  console.log(`/excel called`);
+  const workbook = XLSX.readFile(__dirname + "/../test.xlsx");
+  const sheets = workbook.SheetNames;
+  const dec = sheets[13];
+  console.log(dec);
+  const worksheet = workbook.Sheets[dec];
+  res.json(worksheet);
 });
 
 module.exports = router;
