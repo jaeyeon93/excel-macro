349c8599fe30f824b4602aa784475b41022b9eef
JaeyeonKIM <foodsksms@gmail.com>
2019-11-30 20:58:45 +0900

    field 불러오는거 만들어보기

--- macro.js
+++ macro.js
@@ -13,4 +13,14 @@ console.log(dec);
 
 const worksheet = test_file.Sheets[dec];
 
-console.log(worksheet);
+console.log(worksheet.A1);
+
+// { t: 's',
+//   v: '삼일교회 청년 1부 현금 출납부  (2019.12월-1)',
+//   f: '표지!$M$70&" 현금 출납부  ("&LEFT(표지!E19,4)&"."&L1&"-1)"',
+//   h: '삼일교회 청년 1부 현금 출납부  (2019.12월-1)',
+//   w: '삼일교회 청년 1부 현금 출납부  (2019.12월-1)' }
+// t is
+// h field는 Parse rich text and save HTML
+// w field는 generate formatted text
+// f : save formulae to f field
